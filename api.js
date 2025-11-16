6145608170fd4a4d8ff6c3692014d2107d7b8b26
marlyfr <marlypichol235@gmail.com>
2025-11-15 23:09:57 -0600

    api.js

--- api.js
+++ api.js
@@ -2,20 +2,66 @@ const API_BASE = "https://proyectofinal-1-81b6.onrender.com";
 
 export const Api = {
   get: async (url) => {
-    const res = await fetch(API_BASE + url);
-    if (!res.ok) throw new Error("Error en GET");
-    return res.json();
+    const token = localStorage.getItem("token");
+    const res = await fetch(API_BASE + url, {
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": token ? "Bearer " + token : ""
+      }
+    });
+
+    const data = await res.json();
+    if (!res.ok) throw new Error(data.message || "Error en GET");
+    return data;
   },
 
   post: async (url, body) => {
+    const token = localStorage.getItem("token");
+
     const res = await fetch(API_BASE + url, {
       method: "POST",
-      headers: { "Content-Type": "application/json" },
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": token ? "Bearer " + token : ""
+      },
       body: JSON.stringify(body)
     });
-    if (!res.ok) throw new Error("Error en POST");
-    return res.json();
+
+    const data = await res.json();
+    if (!res.ok) throw new Error(data.message || "Error en POST");
+    return data;
   },
+
+  put: async (url, body) => {
+    const token = localStorage.getItem("token");
+    const res = await fetch(API_BASE + url, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": token ? "Bearer " + token : ""
+      },
+      body: JSON.stringify(body)
+    });
+
+    const data = await res.json();
+    if (!res.ok) throw new Error(data.message || "Error en PUT");
+    return data;
+  },
+
+  del: async (url) => {
+    const token = localStorage.getItem("token");
+    const res = await fetch(API_BASE + url, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": token ? "Bearer " + token : ""
+      }
+    });
+
+    const data = await res.json();
+    if (!res.ok) throw new Error(data.message || "Error en DELETE");
+    return data;
+  }
 };
 
 
